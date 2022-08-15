3928e7f8d541ced51498242f0780e8ac7b6abed7
MewPk <milzaza123@gmail.com>
2022-08-15 11:08:59 +0700

    success lab 1.1  (c language)

--- Lab 2 Pseudo-Code and C Programming/1.c
+++ Lab 2 Pseudo-Code and C Programming/1.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int number, answer, op = 2;
+    printf("Enter number :");
+    scanf("%d", &number);
+    answer = number;
+
+    while (answer > 1)
+    {
+        if (number % op == 0)
+        {
+            number /= op;
+            answer = number;
+            printf("%d", op);
+            if (answer != 1)
+            {
+                printf(" x ");
+            }
+        }
+        else
+        {
+            op += 1;
+        }
+    }
 
     return 0;
 }
