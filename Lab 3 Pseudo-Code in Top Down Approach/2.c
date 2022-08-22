216e983a254d1682b91b92cec6db25ee5dd5105e
MewPk <milzaza123@gmail.com>
2022-08-22 12:52:00 +0700

    successful lab3 (2.c)

---
+++ Lab 3 Pseudo-Code in Top Down Approach/2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char *answer;
+    int px, x = 0;
+    printf("P(x) :");
+    scanf("%d", &px);
+
+    printf("P(%d) = ", px);
+    for (int i = 1; i <= 10000; i++)
+    {
+        if (px == x || px == i)
+        {
+            break;
+        }
+        else if (px % i == 0)
+        {
+            if (i == 1)
+            {
+
+                printf("%d ", i);
+                x += i;
+            }
+            else
+            {
+
+                printf("+ %d ", i);
+                x += i;
+            }
+        }
+    }
+
+    return 0;
+}
