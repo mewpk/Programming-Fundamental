8ae0b9936ada0884627099ec54419ab0364830f4
MewPk <milzaza123@gmail.com>
2022-08-23 15:40:52 +0700

    successful lab3 Yeah

--- Lab 3 Pseudo-Code in Top Down Approach/2.c
+++ Lab 3 Pseudo-Code in Top Down Approach/2.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 
 int main()
-{   
+{
     int sum = 0;
     for (int i = 1; i <= 10000; i++)
     {
         for (int j = 1; j < i; j++)
-        {   
+        {
             if (sum > i)
             {
                 break;
             }
-            
-            if (i % j == 0 )
+
+            if (i % j == 0)
             {
                 sum += j;
             }
-            
         }
-        if (i == 1){
-            
+        if (i == 1)
+        {
+
             printf("P(1) = 1 \n");
         }
-        
-        if (sum == i  )
+
+        if (sum == i)
         {
-            printf("P(%d) = %d \n",i,sum);
+            printf("P(%d) = %d \n", i, sum);
             sum = 0;
         }
-        else{
+        else
+        {
             sum = 0;
         }
-        
-        
     }
-    
+
     return 0;
 }
