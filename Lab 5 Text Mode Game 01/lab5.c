c665c8a3746f82e81043039a674f4368bf7f4f36
MewPk <milzaza123@gmail.com>
2022-09-09 22:04:54 +0700

    edit files lab5 !

--- Lab 5 Text Mode Game 01/lab5.c
+++ Lab 5 Text Mode Game 01/lab5.c
@@ -1,49 +1,56 @@
 #include <stdio.h>
 #include <windows.h>
-#include<conio.h>
+#include <conio.h>
+#include <stdlib.h>
 
-
-void gotoxy(int x, int y)
-{
-    COORD c = {x, y};
+void draw_ship(int x, int y)
+{   
+    
+    COORD c = { x, y };
     SetConsoleCursorPosition(
         GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
-
-void draw_ship()
-{
     printf(" <-0-> ");
+    
 }
 int main()
 {
-
+    system("cls");
     char ch = ' ';
     int x = 38, y = 20;
     draw_ship(x, y);
+    
+  
     do
     {
+        
         if (_kbhit())
-        {
+        {   
             ch = _getch();
-            if (ch == 'a')
+           system("CLS");
+            if (ch == 'a' && x > 0)
             {   
-                gotoxy(--x,y);
-                draw_ship(--x, y);
+                draw_ship(x -= 2, y);
+                
             }
-            if (ch == 'w')
-            {   
-                gotoxy(x,++y);
-                draw_ship(x,++y);
+            else if (ch == 'w' && y > 0)
+            {  
+                draw_ship(x, --y);
+                
             }
-             if (ch == 's')
-            {   
-                gotoxy(x,--y);
-                draw_ship(x,--y);
+            else if (ch == 's' && y < 30)
+            {
+                
+             
+                draw_ship(x, ++y);
+              
             }
-            if (ch == 'd')
-            {   
-                gotoxy(++x,y);
-                draw_ship(++x, y);
+            else if (ch == 'd' && x < 80)
+            {
+                draw_ship(x += 2, y);
+              
+            }
+            else {
+                draw_ship(x, y);
             }
             fflush(stdin);
         }
