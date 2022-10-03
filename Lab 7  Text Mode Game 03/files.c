7b22d03a5011664c70799038ae94491fde778944
MewPk <milzaza123@gmail.com>
2022-10-03 11:00:15 +0700

    successful !! Activity

---
+++ Lab 7  Text Mode Game 03/files.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+
+struct player
+{
+    char name[20];
+    int level;
+    int score;
+};
+
+int main()
+{
+    struct player p[10], *ptr;
+    for (size_t i = 0; i < 10; i++)
+    {
+        printf("Player %d \n", i + 1);
+        printf("Name :");
+        scanf("%s", &p[i].name);
+        printf("Level :");
+        scanf("%d", &p[i].level);
+        printf("Score :");
+        scanf("%d", &p[i].score);
+    }
+    ptr = p;
+
+    FILE *fp;
+
+    fp = fopen("testfile.txt", "w");
+    for (size_t i = 0; i < 10; i++)
+    {
+        fprintf(fp, "%s %d %d \n", ptr->name, ptr->level, ptr->score);
+        ptr++;
+    }
+
+    fclose(fp);
+
+    return 0;
+}
