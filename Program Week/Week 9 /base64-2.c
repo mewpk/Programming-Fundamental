654b67c8897de946d76001ab628259661599bb33
mewpk <milzaza123@gmail.com>
2022-10-17 11:48:57 +0700

    Successful Week 9 !!!

--- Program Week/Week 9 /base64-2.c
+++ Program Week/Week 9 /base64-2.c
@@ -1,101 +1,32 @@
-// C program to encode an ASCII
-// string in Base64 format
 #include <stdio.h>
-#include <stdlib.h>
-#define SIZE 1000
+#include <unistd.h>
 
-// Takes string to be encoded as input
-// and its length and returns encoded string
-char* base64Encoder(char input_str[], int len_str)
+typedef unsigned long UL;
+
+int main(void)
 {
-	// Character set of base64 encoding scheme
-	char char_set[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	
-	// Resultant string
-	char *res_str = (char *) malloc(SIZE * sizeof(char));
-	
-	int index, no_of_bits = 0, padding = 0, val = 0, count = 0, temp;
-	int i, j, k = 0;
-	
-	// Loop takes 3 characters at a time from
-	// input_str and stores it in val
-	for (i = 0; i < len_str; i += 3)
-		{
-			val = 0, count = 0, no_of_bits = 0;
+	const char *alpha =	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+				"abcdefghijklmnopqrstuvwxyz"
+				"0123456789+/";
+	unsigned char c[4];
+	UL u, len, w = 0;
 
-			for (j = i; j < len_str && j <= i + 2; j++)
-			{
-				// binary data of input_str is stored in val
-				val = val << 8;
-				
-				// (A + 0 = A) stores character in val
-				val = val | input_str[j];
-				
-				// calculates how many time loop
-				// ran if "MEN" -> 3 otherwise "ON" -> 2
-				count++;
-			
-			}
+	do {
+		c[1] = c[2] = 0;
 
-			no_of_bits = count * 8;
+		if (!(len = read(fileno(stdin), c, 3))) break;
+		u = (UL)c[0]<<16 | (UL)c[1]<<8 | (UL)c[2];
 
-			// calculates how many "=" to append after res_str.
-			padding = no_of_bits % 3;
+		putchar(alpha[u>>18]);
+		putchar(alpha[u>>12 & 63]);
+		putchar(len < 2 ? '=' : alpha[u>>6 & 63]);
+		putchar(len < 3 ? '=' : alpha[u & 63]);
 
-			// extracts all bits from val (6 at a time)
-			// and find the value of each block
-			while (no_of_bits != 0)
-			{
-				// retrieve the value of each block
-				if (no_of_bits >= 6)
-				{
-					temp = no_of_bits - 6;
-					
-					// binary of 63 is (111111) f
-					index = (val >> temp) & 63;
-					no_of_bits -= 6;		
-				}
-				else
-				{
-					temp = 6 - no_of_bits;
-					
-					// append zeros to right if bits are less than 6
-					index = (val << temp) & 63;
-					no_of_bits = 0;
-				}
-				res_str[k++] = char_set[index];
-			}
-	}
+		if (++w == 19) w = 0, putchar('\n');
+	} while (len == 3);
 
-	// padding is done here
-	for (i = 1; i <= padding; i++)
-	{
-		res_str[k++] = '=';
-	}
+	if (w) putchar('\n');
 
-	res_str[k] = '\0;';
-
-	return res_str;
-
-}
-
-
-// Driver code
-int main()
-{
-  char text[100];
-  char *str = text;
-  scanf("%s",str);
-	char input_str[] = str;
-	int len_str;
-
-	// calculates length of string
-	len_str = sizeof(input_str) / sizeof(input_str[0]);
-	
-	// to exclude '\0' character
-	len_str -= 1;
-
-	printf("Input string is : %s\n", input_str);
-	printf("Encoded string is : %s\n", base64Encoder(input_str, len_str));
 	return 0;
 }
+
